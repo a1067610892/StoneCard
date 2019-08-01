beb809ac41e3e9ade58f27742a1d884e856b5ea4
小辉辉 <cjh>
2019-08-01 11:10:03 +0800

    添加界面图

---
+++ src/Lnterface/Lnterface.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Logo">
+    <img src="../assets/img/1.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  data () {
+    return {
+      domheight: 0
+    }
+  },
+  created () {
+    this.HomePage()
+  },
+  methods: {
+    HomePage () {
+      setTimeout(() => {
+        this.$router.push({name: 'Home', path: '/Home'})
+      }, 3000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Logo {
+ width: 10rem;
+ margin: 0 auto;
+ height: 100%;
+}
+.Logo img {
+  width: 10rem;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/Home/Home'
+import Lnterface from '@/Lnterface/Lnterface'
 
 Vue.use(Router)
 
@@ -8,6 +9,10 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Lnterface',
+      component: Lnterface
+    }, {
+      path: '/Home',
       name: 'Home',
       component: Home
     }
